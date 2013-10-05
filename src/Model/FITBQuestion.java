6c99264e8db5c6476b95f1df5e705bbc65beb948
Jamie <Jamie@Jamie-PC>
2013-10-05 23:29:18 +0100

    Updated data structure a little RE: Sections/subsections etc...

--- src/Model/FITBQuestion.java
+++ src/Model/FITBQuestion.java
@@ -1,34 +1,20 @@
 package Model;
 
 public class FITBQuestion implements Question {
-  String comments;
-  int MarkValue;
-	String Question;
-	String Answer;
-	String ChosenAnswer;
-	
-	FITBQuestion (String GivenQuestion, String GivenAnswer) {
-		Question = GivenQuestion;
-		Answer = GivenAnswer;
-	}
-	
-  public String GetComments() {
-		return comments;
-	}
-	
-	String GetQuestion() {
-		return Question;
-	}
-	
-	void SetAnswer(String chosenAnswer) {
-		ChosenAnswer = chosenAnswer;
-	}
-	
-	public int GetMarkValue() {
-		return MarkValue;
-	}
-	
-	public boolean IsCorrect() {
-		return Answer.equals(ChosenAnswer);
-	}
+
+    String Instructions;
+    String Question;
+
+    FITBQuestion(String GivenQuestion) {
+        Question = GivenQuestion;
+    }
+
+    @Override
+    public String GetInstructions() {
+        return Instructions;
+    }
+
+    String GetQuestion() {
+        return Question;
+    }
 }
