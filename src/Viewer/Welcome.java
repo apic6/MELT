72c19f96a1bdc955536be597d6cbd2dd3b104110
mbaxkmt6 <mbaxkmt6@E-C07KI225B25.cs.man.ac.uk>
2013-10-09 11:04:15 +0100

    add TestWizard view

--- src/Viewer/Welcome.java
+++ src/Viewer/Welcome.java
@@ -48,9 +48,16 @@ public class Welcome extends JPanel{
                   ((Viewer)mainFrame).guiChanger(new Student(mainFrame,model));
             }
         });
-
+        
         teacherButton.setText("I am a Teacher");
 
+        teacherButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                  ((Viewer)mainFrame).guiChanger(new TestWizard(mainFrame,model));
+            }
+        });
+        
         welcomeLogo.setText("");
         
         setLayout(new GridBagLayout());
