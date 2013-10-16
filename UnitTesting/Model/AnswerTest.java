484309aa029028726ebabee0eaa5031af1db3227
mbgm8je3 <mbgm8je3@E-C07KI225A19.cs.man.ac.uk>
2013-10-16 12:44:48 +0100

    Adding tests and fixing code based on results

---
+++ UnitTesting/Model/AnswerTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mbgm8je3
+ */
+public class AnswerTest {
+    
+    public AnswerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getID method, of class Answer.
+     */
+    @Test
+    public void testGetID() {
+        System.out.println("getID");
+        Answer instance = new Answer();
+        
+        int result = instance.getID();
+        assertTrue("ID negative, woops!", result >= 0);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toXML method, of class Answer.
+     */
+    @Test
+    public void testToXML() {
+        System.out.println("toXML");
+        Answer instance = new Answer();
+        String expResult = "Error";
+        String result = instance.toXML();
+        assertEquals("Only result of Ansewr.toXML is 'Error', no other result should happen", expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+}
---
+++ UnitTesting/Model/FITBAnswerTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mbgm8je3
+ */
+public class FITBAnswerTest {
+    private static FITBAnswer instance;
+    
+    public FITBAnswerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        instance = new FITBAnswer(1, "Bob");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getID method, of class FITBAnswer.
+     */
+    @Test
+    public void testGetID() {
+        System.out.println("getID");
+        
+        int expResult = 1;
+        int result = instance.getID();
+        assertEquals("Result is not the same as passed during creation", expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }    
+    
+    /**
+     * Test of getAnswer method, of class FITBAnswer.
+     */
+    @Test
+    public void testGetAnswer() {
+        System.out.println("getAnswer");
+        
+        String expResult = "Bob";
+        String result = instance.getAnswer();
+        assertEquals("Result is not the same as passed during creation", expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toXML method, of class FITBAnswer.
+     */
+    @Test
+    public void testToXML() {
+        System.out.println("toXML");
+        String expResult = "<FITBAnswer>\n<AnswerID>" + 1 + "</AnswerID>\n";
+        expResult += "<Answer>Bob</Answer>\n";
+        expResult += "</FITBAnswer>\n";
+        
+        String result = instance.toXML();
+        assertEquals("Expected result of toXML is different", expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
+}
