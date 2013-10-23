7a90fc2b7d73583c4bdf8a3f088e0c56d1f5e134
mbgm8je3 <mbgm8je3@E-C07KI225A19.cs.man.ac.uk>
2013-10-23 10:43:29 +0100

    Added Package

---
+++ src/Model/StudentSubmission/MCQAnswer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.StudentSubmission;
+
+/**
+ *
+ * @author mbgm8je3
+ */
+public class MCQAnswer extends Answer {
+    int answer;
+    
+    public MCQAnswer(int id, int answer) {
+        this.id = id;
+        this.answer = answer;
+    }
+    
+    public int getAnswer() {
+        return answer;
+    }
+    
+    @Override
+    public String toXML() {
+        String XML = "";
+        XML += "<MCQAnswer>\n";
+        XML += "<AnswerID>" + id + "</AnswerID>\n";
+        XML += "<Answer>" + answer + "</Answer>\n";
+        XML += "</MCQAnswer>\n";
+        return XML;         
+    }
+}
---
+++ src/Model/StudentSubmission/SubmissionSection.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.StudentSubmission;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ *
+ * @author mbgm8je3
+ */
+public class SubmissionSection {
+
+    int sectionID;
+    ArrayList<SubmissionSubSection> subSectionList;
+
+    public SubmissionSection(int id) {
+        this.sectionID = id;
+        subSectionList = new ArrayList<>();
+    }
+
+    public int getID() {
+        return sectionID;
+    }
+
+    public void addSubSection(SubmissionSubSection subSection) {
+        subSectionList.add(subSection);
+    }
+
+    public int getSize() {
+        return subSectionList.size();
+    }
+
+    public SubmissionSubSection getSubSection(int id) {
+        return subSectionList.get(id);
+    }
+
+    public void normalise() {
+        Collections.sort(subSectionList, new Comparator<SubmissionSubSection>() {
+            public int compare(SubmissionSubSection a, SubmissionSubSection b) {
+                return a.getID() - b.getID();
+            }
+        });
+        for (int i = 0; i < subSectionList.size(); i++) {
+            subSectionList.get(i).normalise();
+        }
+    }
+
+    public String toXML() {
+        String XML = "";
+        XML += "<SubmissionSection>\n";
+        XML += "<SectionID>" + sectionID + "</SectionID>\n";
+        for (int i = 0; i < subSectionList.size(); i++) {
+            XML += subSectionList.get(i).toXML() + "\n";
+        }
+        XML += "</SubmissionSection>\n";
+        return XML;
+    }
+}
