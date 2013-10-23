47647249662ebeeb87da3f3bfacb0ad692171f78
mbaxkak4 <mbaxkak4@E-C07KI225B24.cs.man.ac.uk>
2013-10-23 13:50:19 +0100

    a WordCount class and som Marker changes

--- src/Viewer/WordCount.java
+++ src/Viewer/WordCount.java
@@ -9,7 +9,21 @@ package Viewer;
  * @author mbaxkak4
  */
 public class WordCount {
-    
+   private int count=0; 
   //  public WordCount
+ public WordCount (String string){
+     
+     String[] words = string.split(" ");  // split input string on space
+
+for (int i = 0; i < words.length; i++) {  // iterate over array
     
+        count++;
+    
+}
+
+}
+ public int getCount(){
+ 
+ 
+ return count;}
 }
