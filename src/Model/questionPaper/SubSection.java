a4f4e7f8af62aba5732fb4a10786b0660de3046b
mbgm8je3 <mbgm8je3@E-C07KI225A19.cs.man.ac.uk>
2013-10-24 15:53:35 +0100

    adding recursive subsections

--- src/Model/questionPaper/SubSection.java
+++ src/Model/questionPaper/SubSection.java
@@ -21,18 +21,20 @@ public class SubSection {
     String Title;
     String Description;
     String Instructions;
-    CollectionType type = CollectionType.UNDEFINED;
+    CollectionType type;
     ArrayList<Question> questionList;
     ArrayList<SubSection> subSectionList;
 
     public SubSection() {
         questionList = null;
         subSectionList = null;
+        type = CollectionType.UNDEFINED;
     }
 
     public SubSection(String Title, String Description, String Instructions) {
         questionList = null;
         subSectionList = null;
+        type = CollectionType.UNDEFINED;
         this.Title = Title;
         this.Description = Description;
         this.Instructions = Instructions;
@@ -67,36 +69,77 @@ public class SubSection {
     }
 
     public Question getQuestion(int ID) {
-        return questionList.get(ID);
+        if (type == CollectionType.QUESTIONS) {
+            return questionList.get(ID);
+        } else {
+            return null;
+        }
+
     }
 
     public int getNumberOfQuestions() {
-        return questionList.size();
-    }
-
-    public void addQuestion(int position, Question Q) {
-
-        questionList.add(position, Q);
-    }
-
-    public void addQuestion(Question Q) {
-        questionList.add(Q);
-    }
-
-    public void removeQuestion(int position) {
-        questionList.remove(position);
-    }
-
-    public void moveQuestionUp(int QuestionToMove) {
-        if (QuestionToMove <= 0) {
-            return;
-        }
-        if (QuestionToMove >= getNumberOfQuestions()) {
-            return;
+        if (type == CollectionType.QUESTIONS) {
+            return questionList.size();
+        } else {
+            return -1;
         }
 
-        questionList.add(QuestionToMove - 1, getQuestion(QuestionToMove));
-        questionList.remove(QuestionToMove + 1);
+    }
+
+    public boolean addQuestion(int position, Question Q) {
+        if (type == CollectionType.UNDEFINED) {
+            type = CollectionType.QUESTIONS;
+            questionList = new ArrayList<>();
+            questionList.add(position, Q);
+            return true;
+        } else if (type == CollectionType.QUESTIONS) {
+            questionList.add(position, Q);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addQuestion(Question Q) {
+        if (type == CollectionType.UNDEFINED) {
+            type = CollectionType.QUESTIONS;
+            questionList = new ArrayList<>();
+            questionList.add(Q);
+            return true;
+        } else if (type == CollectionType.QUESTIONS) {
+            questionList.add(Q);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeQuestion(int position) {
+        if (type == CollectionType.QUESTIONS) {
+            questionList.remove(position);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean moveQuestionUp(int QuestionToMove) {
+        if (type == CollectionType.QUESTIONS) {
+            if (QuestionToMove <= 0) {
+                return false;
+            }
+            if (QuestionToMove >= getNumberOfQuestions()) {
+                return false;
+            }
+
+            questionList.add(QuestionToMove - 1, getQuestion(QuestionToMove));
+            questionList.remove(QuestionToMove + 1);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public void moveQuestionDown(int QuestionToMove) {
@@ -104,35 +147,76 @@ public class SubSection {
     }
 
     public SubSection getSubSection(int ID) {
-        return subSectionList.get(ID);
+        if (type == CollectionType.SUBSECTIONS) {
+            return subSectionList.get(ID);
+        } else {
+            return null;
+        }
     }
 
     public int getNumberOfSubSections() {
-        return subSectionList.size();
-    }
-
-    public void addSubSection(int position, SubSection s) {
-        subSectionList.add(position, s);
-    }
-
-    public void addSubSection(SubSection s) {
-        subSectionList.add(s);
-    }
-
-    public void removeSubSection(int position) {
-        subSectionList.remove(position);
-    }
-
-    public void moveSubSectionUp(int subSectionToMove) {
-        if (subSectionToMove <= 0) {
-            return;
+        if (type == CollectionType.SUBSECTIONS) {
+            return subSectionList.size();
+        } else {
+            return -1;
         }
-        if (subSectionToMove >= getNumberOfSubSections()) {
-            return;
+    }
+
+    public boolean addSubSection(int position, SubSection s) {
+        if (type == CollectionType.UNDEFINED) {
+            type = CollectionType.SUBSECTIONS;
+            subSectionList = new ArrayList<>();
+            subSectionList.add(position, s);
+            return true;
+        } else if (type == CollectionType.SUBSECTIONS) {
+            subSectionList.add(position, s);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addSubSection(SubSection s) {
+        if (type == CollectionType.UNDEFINED) {
+            type = CollectionType.SUBSECTIONS;
+            subSectionList = new ArrayList<>();
+            subSectionList.add(s);
+            return true;
+        } else if (type == CollectionType.SUBSECTIONS) {
+            subSectionList.add(s);
+            return true;
+        } else {
+            return false;
         }
 
-        subSectionList.add(subSectionToMove - 1, getSubSection(subSectionToMove));
-        subSectionList.remove(subSectionToMove + 1);
+    }
+
+    public boolean removeSubSection(int position) {
+
+        if (type == CollectionType.SUBSECTIONS) {
+            subSectionList.remove(position);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean moveSubSectionUp(int subSectionToMove) {
+        if (type == CollectionType.SUBSECTIONS) {
+            if (subSectionToMove <= 0) {
+                return false;
+            }
+            if (subSectionToMove >= getNumberOfSubSections()) {
+                return false;
+            }
+
+            subSectionList.add(subSectionToMove - 1, getSubSection(subSectionToMove));
+            subSectionList.remove(subSectionToMove + 1);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void moveSubSectionDown(int subSectionToMove) {
@@ -152,7 +236,7 @@ public class SubSection {
                 XML += "SUBSECTIONS";
                 for (int i = 0; i < subSectionList.size(); i++) {
                     XML += subSectionList.get(i).toXML();
-                }                
+                }
                 break;
             case QUESTIONS:
                 XML += "QUESTIONS";
